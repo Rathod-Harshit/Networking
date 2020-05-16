0dcbace026097f69cbf9cc528b0bc768797c7735
Harshit Rathod <56636869+Rathod-Harshit@users.noreply.github.com>
2020-05-16 11:10:07 -0700

    Adding del-switch for two new switches

--- modules/ryu.sh
+++ modules/ryu.sh
@@ -4,5 +4,7 @@ if [[ $1 = "c" ]]; then
     ovs-ofctl -O OpenFlow13 del-flows s3
     ovs-ofctl -O OpenFlow13 del-flows s4
     ovs-ofctl -O OpenFlow13 del-flows s5
+    ovs-ofctl -O OpenFlow13 del-flows s6
+    ovs-ofctl -O OpenFlow13 del-flows s7
 fi
 ryu-manager --observe-links --enable-debugger gui_topology.py ryu_controller.py
